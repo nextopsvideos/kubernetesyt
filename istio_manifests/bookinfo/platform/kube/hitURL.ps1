72e7efb1f4e184888bfc9ce49b71af567c510e2c
Charan Baisetty <charan@nextops.in>
2024-07-25 08:59:03 +0700

    istio manifests

---
+++ istio_manifests/bookinfo/platform/kube/hitURL.ps1
@@ -0,0 +1,17 @@
+﻿$URLs = @(
+  'http://104.155.154.181/productpage'
+)
+
+$minSeconds = 1 * 1
+$maxSeconds = 2 * 1
+
+# Loop forever
+while($true){
+  # Send requests to all 3 urls
+  $URLs |ForEach-Object {
+    (Invoke-WebRequest -Uri $_ | Select Content).content
+  }
+
+  # Sleep for a random duration between 10 and 15 minutes
+  Start-Sleep -Seconds (Get-Random -Min $minSeconds -Max $maxSeconds)
+}
